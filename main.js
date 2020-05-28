8f0a672cc96a2064d97604dd4466e707761cd40d
Bernhard Behrendt <bernhard.bezdek@googlemail.com>
2020-05-28 10:18:32 +0200

    fetch card data

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
-import { cards } from "./modules/cards.js";
-import { createCardOverview } from "./modules/overview.js";
+import {cards} from "./modules/cards.js";
+import {createCardOverview} from "./modules/overview.js";
 import {createControls} from "./modules/controls.js";
 
+cards.then((cardData) => {
+    createCardOverview(cardData);
+    createControls();
+});
 
-createCardOverview(cards)
-createControls();
+
 
