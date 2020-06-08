b9a9fcc7a0b0b0aeb01c9e2155334e1990d70a1f
Kuebler, Philipp <philipp.kuebler@aoe.com>
2020-06-08 11:12:59 +0200

    fixed border behavior when card selected

--- dist/main.fb6bbcaf.js
+++ dist/main.fb6bbcaf.js
@@ -171,11 +171,11 @@ function createCardOverview(allCards) {
 function markSelectedCardAndRefreshOverview(event) {
   if (sessionStorage.getItem('selectedCardId')) {
     var previousSelectedCard = document.getElementById(sessionStorage.getItem('selectedCardId'));
-    previousSelectedCard.className = 'card';
+    previousSelectedCard.className = 'card col border border-primary rounded';
   }
 
   var selectedCard = document.getElementById(event.currentTarget.id);
-  selectedCard.className = 'card selected';
+  selectedCard.className = 'card col border border-warning rounded';
   sessionStorage.setItem('selectedCardId', event.currentTarget.id);
 }
 },{}],"js/modules/cards.js":[function(require,module,exports) {
